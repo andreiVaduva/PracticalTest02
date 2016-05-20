4ce2c85637563e4c25926ec3cbb204b2365c9784
andreiVaduva <vaduva.andrei.iustin@gmail.com>
2016-05-20 15:38:44 +0300

    Configured utilities

---
+++ PracticalTest02/app/src/main/java/practicaltest02/eim/systems/cs/pub/ro/practicaltest02/Constants.java
@@ -0,0 +1,22 @@
+package practicaltest02.eim.systems.cs.pub.ro.practicaltest02;
+
+/**
+ * Created by andrei on 20.05.2016.
+ */
+public interface Constants {
+
+    final public static String TAG = "[PracticalTest02]";
+
+    final public static boolean DEBUG = true;
+
+    final public static String WEB_SERVICE_ADDRESS = "https://www.timeapi.org/utc/now";
+
+    final public static String EMPTY_STRING = "";
+
+    final public static String QUERY_ATTRIBUTE = "query";
+
+    final public static String SCRIPT_TAG = "script";
+
+    final public static String CURRENT_OBSERVATION = "current_observation";
+
+}
